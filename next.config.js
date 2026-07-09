a892e2ac304d179f99a037e8721ebe6198489744
David Kwak <mireoksessi@gmail.com>
2026-07-09 03:28:08 -0400

    Ignore ESLint during production builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["pdf-parse-new"],
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
