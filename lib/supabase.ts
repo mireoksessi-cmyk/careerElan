176a7c27047bac195754a0f1a1934ce63a9207e6
David Kwak <mireoksessi@gmail.com>
2026-07-11 21:59:47 -0400

    Checkpoint: OAuth callback cookie debugging

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,10 @@
 import { createBrowserClient } from "@supabase/ssr";
 
+console.log(
+  "SUPABASE URL =",
+  process.env.NEXT_PUBLIC_SUPABASE_URL
+);
+
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
